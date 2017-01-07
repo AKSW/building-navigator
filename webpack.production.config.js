05692e37c2c410fdb436f91263a35e84d774042b
Simeon Ackermann <s.ackermann@mail.de>
2017-01-07 12:02:49 +0100

    Several prototype updates. Ready to run from dist.

---
+++ webpack.production.config.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var webpack = require('webpack');
+
+const PATHS = {
+  dist: path.join(__dirname, 'dist')
+};
+
+module.exports = {
+    devtool: 'cheap-module-source-map',
+    context: path.resolve(__dirname),
+    entry: './src/main.js',    
+    //entry: ['whatwg-fetch', './src/main.js'],
+    output: {
+        path: PATHS.dist,
+        publicPath: '/',
+        filename: '/main.min.js',
+    },
+    resolve: {
+        root: path.resolve(__dirname),
+    },
+    historyApiFallback: {
+        index: '/dist/',
+    },
+    node: {
+        fs: 'empty'
+    },
+    externals: {
+        sqlite3: 'sqlite3'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            },
+        ],
+        loaders: [
+         {
+           test: /\.js?$/,
+           loader: 'babel-loader',
+           exclude: /node_modules/,
+           query: {
+             presets: ['es2015', 'react']
+           }
+         },
+         {
+            test: /\.css/,
+            loader: 'style!css?modules',
+            include: __dirname + '/src'
+          }
+       ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+    ],
+};
