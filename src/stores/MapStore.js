7559de0f5f569d32664cd8cb10e77091a456db6a
Simeon Ackermann <s.ackermann@mail.de>
2017-03-24 10:57:35 +0100

    Add node ref and bounds config

--- src/stores/MapStore.js
+++ src/stores/MapStore.js
@@ -2,13 +2,34 @@ class MapStore {
     constructor(logger) {
         this.logger = logger;
 
+        this.node = null;
+
         this.config = {
+            bounds: {
+                northEast: {
+                    latitude: 999,
+                    longitude: 999
+                },
+                southWest: {
+                    latitude: 0,
+                    longitude: 0
+                }
+            },
             center: {
                 latitude: 51.3412,
                 longitude: 12.3747
             },
             zoom: 14
         }
+    }
+
+    setNode(node) {
+        this.node = node;
+    }
+
+    getNode() {
+        // document.getElementById('map')
+        return this.node;
     }
 
     /**
@@ -24,12 +45,26 @@ class MapStore {
     /**
      * Updates the map config center and zoom
      *
+     * @param {Object} Bounds with northEast and southWest Objects and latitude and longitude
      * @param {Object} Center with latitude and longitude
      * @param {Integer} New zoom
      */
-    updateMapConfig(center, zoom) {
+    updateMapConfig(bounds, center, zoom) {
+        this.updateBounds(bounds.northEast, bounds.southWest);
         this.updateCenter(center.latitude, center.longitude);
         this.updateZoom(zoom);
+    }
+
+    /**
+     * Updates the bounds
+     *
+     * @param {Object} northWest with latitude and longitude as float values
+     * @param {Object} southWest with latitude and longitude as float values
+     */
+    updateBounds(northEast, southWest) {
+        this.update('bounds', {
+            northEast, southWest
+        });
     }
 
     /**
