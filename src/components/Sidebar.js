8205547cc5e76785fa8fc36104744b2f9a595a42
Simeon Ackermann <s.ackermann@mail.de>
2017-03-24 10:52:10 +0100

    Add sidebar toggler

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Swipeable from 'react-swipeable';
+import {Button} from 'react-bootstrap';
 
 import Search from './sidebar/Search';
 import Results from './sidebar/Results';
@@ -10,29 +12,63 @@ class Sidebar extends React.Component {
         this.state = {
             sidebarRoute: 'search',
             stores: props.stores,
-            filters: props.stores.filterStore.getAll(),
         };
+
+        this.handleToggleSidebar = this.handleToggleSidebar.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
         this.setState({
             sidebarRoute: nextProps.stores.uiStore.get('sidebarRoute'),
-            filters: nextProps.stores.filterStore.getAll()
+            stores: nextProps.stores,
         });
+    }
+
+    handleToggleSidebar(e) {
+        super.handleEvent({
+            action: 'toggle-sidebar',
+        });
+        e.preventDefault();
     }
 
     render() {
         const sidebarClass = this.state.stores.uiStore.get('globalDisability') === "blind" ? "sidebar sidebar-full" : "sidebar";
-        return (
-            <div className={sidebarClass}>
-                {this.state.sidebarRoute === 'search' &&
-                    <Search filters={this.state.filters} />
-                }
-                {this.state.sidebarRoute === 'results' &&
-                    <Results stores={this.state.stores} />
-                }
+
+        const sidebarHtml = this.state.stores.uiStore.get('sidebarIsVisible') ? (
+            <Swipeable onSwipedLeft={this.handleToggleSidebar}>
+                <div className={sidebarClass}>
+                    {this.state.sidebarRoute === 'search' &&
+                        <Search filters={this.state.stores.filterStore.getAll()} />
+                    }
+                    {this.state.sidebarRoute === 'results' &&
+                        <Results stores={this.state.stores} />
+                    }
+                </div>
+                <div className="btn-toggle-sidebar btn-hide-sidebar">
+                    <Button
+                        bsClass="btn btn-lg btn-default pull-right"
+                        title="Seitenleiste ausblenden"
+                        onClick={this.handleToggleSidebar}
+                    >
+                        <i className="fa fa-angle-double-left" aria-hidden={true}></i>
+                    </Button>
+                </div>
+            </Swipeable>
+        ) : (
+            <div className="btn-toggle-sidebar btn-show-sidebar">
+                <Button
+                    bsStyle="default"
+                    bsSize="large"
+                    aria-label="Seitenleiste einblenden"
+                    title="Seitenleiste einblenden"
+                    onClick={this.handleToggleSidebar}
+                >
+                    <i className="fa fa-angle-double-right" aria-hidden={true}></i>
+                </Button>
             </div>
         );
+
+        return (sidebarHtml);
     }
 }
 
