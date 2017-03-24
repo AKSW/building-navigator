f01665bdfc8dfc7520c522a3a2ffed6d81be1d13
Simeon Ackermann <s.ackermann@mail.de>
2017-03-24 10:48:24 +0100

    Add eventHandler initiation, remove smallView detection

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Logger from './utils/Logger';
 import * as Store from './stores';
+import EventHandler from './EventHandler';
 import BuildingNavigator from './BuildingNavigator';
 
 /**
@@ -16,8 +17,11 @@ const runBuildingNavigator = (config) => {
     if (! ('container' in config)) {
         throw new Error('Error: "container" id not found');
     }
+    const appEl = document.getElementById(config.container);
 
+    // init logger
     const loggerMode = process.env.NODE_ENV !== 'production' ? 'development' : 'production';
+    // init stores
     const logger = new Logger(loggerMode);
     const stores = {
         buildingStore: new Store.BuildingStore(logger),
@@ -25,10 +29,27 @@ const runBuildingNavigator = (config) => {
         mapStore: new Store.MapStore(logger),
         uiStore: new Store.UIStore(logger)
     };
-    //const router = new Router(logger);
-    //router.createRoutes();
+    // init event handler
+    const eventHandler = new EventHandler(stores, logger);
 
-    ReactDOM.render( <BuildingNavigator stores={stores} logger={logger} />, document.getElementById(config.container) );
+    // add userconfig to uiStore
+    eventHandler.handleEvent({
+        action: 'update-ui-config',
+        payload: {key: 'userConfig', value: config}
+    });
+
+    // set isSmallView to uiStore, we need the timout, otherwise offsetWidth is 0
+    /*window.setTimeout(() => {
+        if (appEl.offsetWidth <= stores.uiStore.get('smallViewMax')) {
+            eventHandler.handleEvent({
+                action: 'update-ui-config',
+                payload: {key: 'isSmallView', value: true}
+            });
+        }
+    }, 0);*/
+
+    // render the app
+    ReactDOM.render( <BuildingNavigator stores={stores} logger={logger} eventHandler={eventHandler} />, appEl );
 }
 
 module.exports = runBuildingNavigator;
