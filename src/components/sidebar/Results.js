ce2d0c1dd36dc51fbeb8b928bb6a56e34ea942c9
Simeon Ackermann <s.ackermann@mail.de>
2017-03-24 11:07:55 +0100

    Implement results pager

--- src/components/sidebar/Results.js
+++ src/components/sidebar/Results.js
@@ -1,29 +1,41 @@
 import React from 'react';
-import {Grid, Row, Col, Button, Glyphicon} from 'react-bootstrap';
+import {Grid, Row, Col, Button, Pager} from 'react-bootstrap';
+
 import ResultsEntry from './ResultsEntry';
+import {getElement} from '../../utils/GuiUtils'
 
 class Results extends React.Component {
     constructor(props) {
         super();
 
+        let buildings = props.stores.buildingStore.getVisibles();
+
         this.state = {
-            buildings: []
+            stores: props.stores,
+            buildings: buildings.slice(
+                props.stores.uiStore.get('resultsStart'),
+                props.stores.uiStore.get('resultsStart') + props.stores.uiStore.get('resultsSteps')
+            )
         }
 
         this.handleBackToSearch = this.handleBackToSearch.bind(this);
+        this.handleNextResults = this.handleNextResults.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
         let buildings = nextProps.stores.buildingStore.getVisibles();
-        buildings = nextProps.stores.buildingStore.sortByDistance({
+        /*buildings = nextProps.stores.buildingStore.sortByDistance({
             buildings: buildings,
             latitude: nextProps.stores.mapStore.get('center').latitude,
             longitude: nextProps.stores.mapStore.get('center').longitude
-        });
-        buildings = buildings.slice(0, 10);
+        });*/
         this.setState({
-            buildings: buildings
-        });        
+            stores: nextProps.stores,
+            buildings: buildings.slice(
+                nextProps.stores.uiStore.get('resultsStart'),
+                nextProps.stores.uiStore.get('resultsStart') + nextProps.stores.uiStore.get('resultsSteps')
+            )
+        });
     }
 
     handleBackToSearch(e) {
@@ -36,24 +48,92 @@ class Results extends React.Component {
         });
     }
 
+    handlePrevResults(e) {
+        super.handleEvent({
+            action: 'prev-results'
+        });
+    }
+
+    handleNextResults(e) {
+        super.handleEvent({
+            action: 'next-results'
+        });
+        getElement(this.state.stores.uiStore.get('userConfig').container, '.sidebar').then((sidebar) => {
+            sidebar.scrollTop = 0;
+        })
+    }
+
     render() {
+        const visiblesLength = this.state.stores.buildingStore.getVisibles().length;
+        const resultsStart = this.state.stores.uiStore.get('resultsStart');
+        const resultsSteps = this.state.stores.uiStore.get('resultsSteps');
+
+        const resultsPager = (
+            <ul className="pager">
+                {resultsStart > 0 &&
+                    <li className="previous">
+                        <a role="button" href="#" onClick={this.handlePrevResults}>
+                            <i className="fa fa-chevron-left" aria-hidden="true"></i>&nbsp; vorherige
+                        </a>
+                    </li>
+                }
+                <li><span>{(resultsStart+resultsSteps)/resultsSteps} von {Math.ceil(visiblesLength/resultsSteps)}</span></li>
+                {(resultsStart + resultsSteps) < visiblesLength &&
+                    <li className="next">
+                        <a role="button" href="#" onClick={this.handleNextResults}>
+                            nächste <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                }
+            </ul>
+        );
+
         return (
-            <div className="results">
+            <div className="results" lang="de">
                 <Row>
-                    <Col md={12}>
+                    <Col xs={6} className="back-to-search-wrapper">
                         <Button className="btn btn-primary btn-lg" title="Zurück zur Suche" onClick={this.handleBackToSearch}>
-                            <span><Glyphicon glyph="search" aria-hidden="true" /> Zur Suche</span>
+                            <span><i className="fa fa-search" aria-hidden={true}></i> Zur Suche</span>
                         </Button>
                     </Col>
-                    <Col md={12}>
-                        <p>
-                            {this.state.buildings.length} Ergebnisse von {this.props.stores.buildingStore.getVisibles().length}
-                        </p>
-                    </Col>
                 </Row>
-                {this.state.buildings.map((building, bid) =>
-                    <ResultsEntry key={bid} building={building} />
-                )}
+                {this.state.buildings.map((building, bid) => {
+                    return (
+                        <ResultsEntry key={bid} building={building} stores={this.state.stores} />
+                    );
+                })}
+
+                {visiblesLength > resultsSteps &&
+                    <Row>
+                        {resultsPager}
+                    </Row>
+                }
+
+                {/*<Row>
+                    <Pager>
+                        {resultsStart > 0 &&
+                            <Pager.Item href="#" onClick={this.handlePrevResults}>
+                                <i className="fa fa-chevron-left" aria-hidden="true"></i> Vorherige Ergebnisse
+                            </Pager.Item>
+                        }
+                        {(resultsStart + resultsSteps) < visiblesLength &&
+                            <Pager.Item href="#" onClick={this.handleNextResults}>
+                                Nächste Ergebnisse <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                            </Pager.Item>
+                        }
+                  </Pager>
+                </Row>*/}
+
+                {this.state.buildings.length > 3 &&
+                    <Row>
+                        <br />
+                        <Col md={12} className="back-to-search-wrapper">
+                            <Button className="btn btn-primary btn-lg" title="Zurück zur Suche" onClick={this.handleBackToSearch}>
+                                <span><i className="fa fa-search" aria-hidden={true}></i> Zur Suche</span>
+                            </Button>
+                        </Col>
+                    </Row>
+                }
             </div>
         );
     }
