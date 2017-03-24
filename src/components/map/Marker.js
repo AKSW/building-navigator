e04c68dfa941163c5baf153be92896adb625ac25
Simeon Ackermann <s.ackermann@mail.de>
2017-03-24 11:13:34 +0100

    Implement multi marker (see #25), popup only for bigger screens and more...

--- src/components/map/Marker.js
+++ src/components/map/Marker.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import L from 'leaflet';
 import {Marker as OSMarker, Icon, Popup} from 'react-leaflet';
+import {
+    Button
+} from 'react-bootstrap'
+
+import A11yIcon from '../A11yIcon';
+import {getElement} from '../../utils/GuiUtils'
 
 const normalIcon = L.icon({
     iconUrl: './images/marker-icon.png',
@@ -13,49 +19,176 @@ const smallIcon = L.icon({
     iconUrl: './images/small-marker-icon.png',
     iconSize: [15, 16],
     iconAnchor: [7, 16],
-    popupAnchor:  [0, -6]
+    popupAnchor:  [0, -46]
+});
+
+const selectedIcon = L.icon({
+    iconUrl: './images/selected-marker-icon.png',
+    iconSize: [31, 51],
+    iconAnchor: [15, 51],
+    popupAnchor:  [0, -45]
 });
 
 class Marker extends React.Component {
     constructor(props) {
         super();
-        
+
         this.state = {
-            building: props.building,
+            stores: props.stores,
+            marker: props.marker,
+            currentBuildingId: 0,
             zoom: props.zoom
         };
 
         this.handleShowDetails = this.handleShowDetails.bind(this);
+        this.handleClickMarker = this.handleClickMarker.bind(this);
+        this.handleSetCurrentBuildingId = this.handleSetCurrentBuildingId.bind(this);
+        this.handleSetSelectedOnMap = this.handleSetSelectedOnMap.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({zoom: nextProps.zoom});
+        let currentBuildingId = 0;
+        nextProps.marker.buildings.forEach((building, bid) => {
+            if (building.selectOnMap) {
+                currentBuildingId = bid;
+            }
+        })
+
+        this.setState({
+            stores: nextProps.stores,
+            currentBuildingId: currentBuildingId,
+            marker: nextProps.marker,
+            zoom: nextProps.zoom,
+        });
     }
 
-    handleShowDetails(e) {
+    handleShowDetails(e, buildingId) {
         super.handleEvent({
-            action: 'load-building-data',
+            action: 'may-load-building-data',
             payload: {
-                buildingId: e.target.getAttribute('data-building-id'),
+                buildingId: buildingId,
+            }
+        }).then(() => {
+            super.handleEvent({
+                action: 'show-building-details',
+                payload: {
+                    buildingId: buildingId,
+                }
+            });
+            super.handleEvent({
+                action: 'show-sidebar'
+            });
+            super.handleEvent({
+                action: 'apply-filters'
+            });
+            super.handleEvent({
+                action: 'update-ui-config',
+                payload: {
+                    key: 'sidebarRoute',
+                    value: 'results'
+                }
+            });
+            getElement(this.state.stores.uiStore.get('userConfig').container, `[id="result-entry-${buildingId}"]`).then((entry) => {
+                entry.scrollIntoView();
+            })
+        });
+    }
+
+    handleClickMarker(e, building) {
+        // for small devices: immediately open building details in sidebar
+        if (this.state.stores.uiStore.get('isSmallView')) {
+            this.handleShowDetails(e, building.id);
+        }
+        super.handleEvent({
+            action: 'set-selected-on-map',
+            payload: {
+                buildingId: building.id,
             }
         });
     }
 
+    handleSetSelectedOnMap(e, building) {
+        super.handleEvent({
+            action: 'set-selected-on-map',
+            payload: {
+                buildingId: building.id,
+            }
+        });
+    }
+
+    handleSetCurrentBuildingId(e, value) {
+        this.setState({
+            currentBuildingId: value
+        });
+    }
+
     render() {
-        const building = this.state.building;
-        const position = [building.latitude, building.longitude];
-        //console.log('Marker zoom: ', this.state.zoom);
-        const icon = this.state.zoom < 15 ? smallIcon : normalIcon;
+        const isSmallView = this.state.stores.uiStore.get('isSmallView');
+        const marker = this.state.marker;
+        const currentBuilding = marker.buildings[this.state.currentBuildingId];
+        const position = [currentBuilding.latitude, currentBuilding.longitude];
+
+        let icon = normalIcon;
+        if (currentBuilding.selectOnMap === true) {
+            icon = selectedIcon;
+        }
+        else if (this.state.zoom < 15) {
+            icon = smallIcon
+        }
+
+        const a11yIcons = new A11yIcon({building: currentBuilding});
+
+        const multiMarkerNav = (
+            <ul className="pager">
+                {this.state.currentBuildingId > 0 &&
+                    <li className="previous">
+                        <a role="button" href="#" onClick={e => this.handleSetSelectedOnMap(e, marker.buildings[this.state.currentBuildingId-1])}>
+                            <i className="fa fa-chevron-left" aria-hidden="true"></i>&nbsp; vorheriges
+                        </a>
+                    </li>
+                }
+                {marker.buildings.length > 1 &&
+                    <li><span>{this.state.currentBuildingId+1} von {marker.buildings.length}</span></li>
+                }
+                {marker.buildings.length > 1 && (this.state.currentBuildingId+1) < marker.buildings.length &&
+                    <li className="next">
+                        <a role="button" href="#" onClick={e => this.handleSetSelectedOnMap(e, marker.buildings[this.state.currentBuildingId+1])}>
+                            nächstes <i className="fa fa-chevron-right" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                }
+            </ul>
+        );
 
         return (
             <OSMarker
                 position={position}
                 icon={icon}
+                onClick={e => this.handleClickMarker(e, currentBuilding)}
                 >
                 <Popup>
                     <span>
-                        <h3>{building.title}</h3>
-                        <a href="#" data-building-id={building.id} onClick={this.handleShowDetails}>Details</a>
+                        {!isSmallView &&
+                            <div id={`popup-${currentBuilding.id}`} className="popup">
+                                <h3>{currentBuilding.title}</h3>
+
+                                <ul className="a11yIcons-list">
+                                    {a11yIcons.getAll().map((entry, id) => {
+                                        return (<li key={id}>
+                                            {a11yIcons.icon(entry)}
+                                        </li>);
+                                    })}
+                                </ul>
+
+                                <Button className="btn-lg" onClick={e => this.handleShowDetails(e, currentBuilding.id)}>
+                                    <i className="fa fa-th-list"></i> Details
+                                </Button>
+
+                                {marker.buildings.length > 1 &&
+                                    <div className="multi-marker-nav">{multiMarkerNav}</div>
+                                }
+                            </div>
+                        }
                     </span>
                 </Popup>
             </OSMarker>
