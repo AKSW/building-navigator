7eedb61374d4bdcb7fa1a4bbc5244200029e9f91
Simeon Ackermann <amseon@web.de>
2018-11-29 09:51:59 +0100

    Remove 'react-tap-event-plugin' as it causes an error on production build and is deprecated
    See https://www.npmjs.com/package/react-tap-event-plugin

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Logger from './utils/Logger';
 import * as Store from './stores';
 import EventHandler from './EventHandler';
 import BuildingNavigator from './BuildingNavigator';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 /**
  * Entry file to init and render the app.
